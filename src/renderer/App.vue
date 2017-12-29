9f600c85b7939ae0a5b9b11bcc1650dfb0e64cab
alex zhang <gzane24@gmail.com>
2017-12-29 09:15:52 +0800

    add project

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'lucky-draw-app',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
