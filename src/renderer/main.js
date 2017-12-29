88f41063a5461044cb4bef1a6dac3522b5801ff3
alex zhang <gzane24@gmail.com>
2017-12-29 15:34:41 +0800

    提交代码

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue';
 import axios from 'axios';
+import Dexie from 'dexie';
 
-import App from './App';
+import App from './App.vue';
 import router from './router';
 import store from './store';
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
+
+Vue.db = new Dexie('lucky-draw-app');
+Vue.db.version(1)
+  .stores({
+    friends: '++id,name,age'
+  });
 
 /* eslint-disable no-new */
 new Vue({
