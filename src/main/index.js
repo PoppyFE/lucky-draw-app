ddfa496a3255a3a7d1b4dec645282fcdb4df5415
alex zhang <gzane24@gmail.com>
2018-01-08 13:13:22 +0800

    添加dev

--- src/main/index.js
+++ src/main/index.js
@@ -35,7 +35,7 @@ function createWindow() {
 
   mainWindow.loadURL(winURL);
 
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', () => {
     mainWindow = null;
